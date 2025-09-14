2284ffa614d9bff042e54ef4759891ae4bea6339
Gustavo Martins <gustavosm994@gmail.com>
2025-09-14 00:05:15 -0300

    feat: add middleware on create user

--- apps/server/src/controllers/users/routes.ts
+++ apps/server/src/controllers/users/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { createUserRoute } from "./create-user";
 import { authenticateUser } from "./authenticate-user";
+import { verifyJWT } from "../../middleware/verify-jwt";
+import { requireProfessorOrAdmin } from "../../middleware/require-professor-or-admin";
 
 const usersRoutes: Router = Router();
 
-usersRoutes.post('/', createUserRoute)
+usersRoutes.post('/', verifyJWT, requireProfessorOrAdmin, createUserRoute)
 usersRoutes.post('/authenticate', authenticateUser)
 
 export { usersRoutes }
