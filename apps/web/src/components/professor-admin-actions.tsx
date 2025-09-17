514ce5af4e9cb47432dc35c182e9bf4d381bf220
Gustavo Martins <gustavosm994@gmail.com>
2025-09-16 23:23:57 -0300

    fix: professor and admin actions

--- apps/web/src/components/professor-admin-actions.tsx
+++ apps/web/src/components/professor-admin-actions.tsx
@@ -1,20 +1,27 @@
+'use client'
+
 import { Flex, Button } from "@chakra-ui/react";
 import { LuPlus } from "react-icons/lu";
 import { CreateImcForm } from "./create-imc-form";
 import { CreateUserForm } from "./create-user";
 import { useDialogStore } from "../app/store/dialog-store";
+import { useAuthStore } from "../app/store/auth";
 
 export function ProfessorOrAdminActions() {
   const { openDialog } = useDialogStore()
+  const user = useAuthStore(({ user }) => user)
+  const isAdmin = user?.role === "ADMIN"
 
   return (
     <Flex alignItems={'center'} justifyContent={'space-between'} mb='1.5rem'>
-      <Button bg='blue.500' onClick={() =>
-        openDialog("Adicionar usuário", <CreateUserForm />)
-      }>
-        <LuPlus />
-        Usuário
-      </Button>
+      {isAdmin ? (
+        <Button bg='blue.500' onClick={() =>
+          openDialog("Adicionar usuário", <CreateUserForm />)
+        }>
+          <LuPlus />
+          Usuário
+        </Button>
+      ) : null}
       <Button bg='blue.500' onClick={() =>
         openDialog("Adicionar avaliação", <CreateImcForm />)
       }>
--- apps/web/src/components/table/imc-table.tsx
+++ apps/web/src/components/table/imc-table.tsx
@@ -1,23 +1,16 @@
 'use client'
 
-import { Button, Flex, Table } from "@chakra-ui/react";
+import { Table } from "@chakra-ui/react";
 import { IMCTableRow } from "./imc-table-row";
 import { ImcFilter } from "./imc-filter";
 import { useQuery } from "@tanstack/react-query";
 import { getImcs } from "../../lib/api/get-imcs";
-import { LuPlus } from "react-icons/lu";
-import { useDialogStore } from "../../app/store/dialog-store";
-import { CreateImcForm } from "../create-imc-form";
 import { useSearchParams } from "next/navigation";
-import { CreateUserForm } from "../create-user";
-import { useAuthStore } from "../../app/store/auth";
 import { ProfessorOrAdminActions } from "../professor-admin-actions";
 
 export function ImcTable() {
   const searchParams = useSearchParams();
   const name = searchParams.get('name') ?? '';
-  const user = useAuthStore(({ user }) => user)
-  const isAdmin = user?.role === "ADMIN"
 
   const { data, isLoading } = useQuery({
     queryKey: ['imcs', name],
@@ -29,7 +22,7 @@ export function ImcTable() {
   return (
     <>
       <ImcFilter />
-      {isAdmin ? (<ProfessorOrAdminActions />) : null}
+      <ProfessorOrAdminActions />
       <Table.Root variant='outline' size="lg">
         <Table.Header bg="gray.50">
           <Table.Row>
